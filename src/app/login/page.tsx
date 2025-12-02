4ca1f3e19a107effd394938873f64d3df68b1fa9
doreeday <christinadoreen.geslani,bsit@gmail.com>
2025-12-02 11:38:57 +0800

    setting up

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Login() {
+  return (
+   <section>
+    <div> 
+        <link href="/"> Home </link>
+    </div>
+    <div> Login Page</div>
+   </section>
+  );
+}
